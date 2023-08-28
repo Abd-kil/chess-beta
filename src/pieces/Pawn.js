746987d7fed013512c7dd0a03e4c3c25399af6f5
abdulrahman <abood.kil2003123@gmail.com>
2023-08-29 00:19:43 +0300

    fix a little bug

--- src/pieces/Pawn.js
+++ src/pieces/Pawn.js
@@ -1,5 +1,5 @@
 import blackPawn from '../images/black-pawn.png';
-import whitPawn from '../images/white-bishop.png';
+import whitPawn from '../images/white-pawn.png';
 function Pawn(props){
     const pawnImg = props.color === 'black'?blackPawn:whitPawn;
     return <img src={pawnImg} alt=''/>
